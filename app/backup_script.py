67fec3a807db4d61a2b75e19387a5c69c8faa092
Nick Hopgood <nick.hopgood@sap.com>
2023-10-09 14:54:25 +0100

    update

--- app/backup_script.py
+++ app/backup_script.py
@@ -53,7 +53,6 @@ def get_all_backups(environment):
 def get_napalm_backups(task, hash=None):
     try:
         backup_result = task.run(task=napalm_get, getters=["config"])
-        print(backup_result)
         config = backup_result.result["config"]["running"]
         save_config_to_file(hostname=task.host.name, config=config, hash=str(hash))
         #shutil.copy(f"./gold_config/{hostname}.txt", f"./crq_configs/{hostname}.txt")
